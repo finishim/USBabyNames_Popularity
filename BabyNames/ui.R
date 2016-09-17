e0d33383bc1df0bf62194c4f6115bde0c3e45c43
Nazmi Anik <nazmianik@gmail.com>
2016-09-17 15:00:52 -0400

    Add Instructions to user

--- BabyNames/ui.R
+++ BabyNames/ui.R
@@ -18,6 +18,7 @@ shinyUI(fluidPage(
   # Sidebar with a text, radio button and slider input 
   sidebarLayout(
       sidebarPanel(
+          h3("Please fill out name, select gender and choose the year range below, to see how popular the name was throughout the years."),
           textInput(inputId="name", label = "Enter Name:"),
           radioButtons("sex", "Choose Gender:",c("Male" = "M","Female" = "F")),
           sliderInput("range", "Choose Year:",min = 1880, max = 2015, value = c(1950,2015))
